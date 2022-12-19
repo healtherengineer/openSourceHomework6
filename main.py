a821e9e3ce513b4e54f6354df3f6db6cdc337470
Mustafa ZAİMOĞLU <mustafazaimoglu1@gmail.com>
2022-12-19 18:42:38 +0300

    new function added to draw shapes

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import cv2 as cv
 import numpy as np
-from imageio import imread, imwrite
+# from imageio import imread, imwrite
 
+def myCrazyFunctionThatDrawsShapes(src_img):
+    pts = [(50, 50), (80, 150), (170, 30)]
+    cv.polylines(src_img, np.array([pts]), True, (255,0,255), 5) # TRIANGLE
+    cv.circle(src_img, (150, 150), 40, (255, 0, 0), 3) # CIRCLE
+    cv.rectangle(src_img, (10, 10), (200, 200), (0, 255, 0), 5) # RECTANGLE
+    cv.putText(src_img, 'ZAIMOGLU', (50, 250),cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, cv.LINE_AA) # ALSO MY LAST NAME
+    return src_img
 
 original = cv.imread("./resources/eifellTower.jpg")
+
+result_image = myCrazyFunctionThatDrawsShapes(original.copy())
+cv.imshow("ZAIMOGLU",result_image)
 
 cv.imshow("Eifell",original)
 cv.waitKey()
